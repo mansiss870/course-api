431ca8036d4d6a5efc841c343aeb38e0aa23995b
Mansi Sharma <mansiss870@gmail.com>
2022-03-30 19:43:23 +0530

    added courseApiApp and HelloController

---
+++ src/main/java/io/msharma/springbootstarter/CourseApiApp.java
@@ -0,0 +1,11 @@
+package io.msharma.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+    public static void main(String[] gg){
+        SpringApplication.run(CourseApiApp.class,gg);
+    }
+}
